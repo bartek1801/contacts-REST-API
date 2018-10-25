f412a579b4ed8bf08385bed8e9ae5ed2092ebcd8
bartlomiej.lech <bartlomiej.lech@britenet.com.pl>
2018-10-25 21:59:26 +0200

    UpdatePersonHandler refactor

--- src/main/java/lech/bartlomiej/contacts/api/handlers/UpdatePersonDetailsHandler.java
+++ src/main/java/lech/bartlomiej/contacts/api/handlers/UpdatePersonDetailsHandler.java
@@ -1,13 +1,10 @@
 package lech.bartlomiej.contacts.api.handlers;
 
-import lech.bartlomiej.contacts.domain.Person;
 import lech.bartlomiej.contacts.domain.commands.UpdatePersonDetailsCommand;
 import lech.bartlomiej.contacts.domain.repositories.PersonRepository;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
 
 @Component
 public class UpdatePersonDetailsHandler {
@@ -20,12 +17,14 @@ public class UpdatePersonDetailsHandler {
     }
 
     @Transactional
-    public void handle(UpdatePersonDetailsCommand command){
-        Optional<Person> person = personRepository.findByPesel(command.getPesel());
-        if (person.isPresent()){
-            person.get().updateDetails(command);
-            personRepository.save(person.get());
-        }
+    public void handle(UpdatePersonDetailsCommand command) {
+
+        personRepository.findByPesel(command.getPesel())
+                .ifPresent(
+                        person -> {
+                            person.updateDetails(command);
+                            personRepository.save(person);
+                        });
     }
 
 }
